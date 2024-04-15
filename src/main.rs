1c105ab6edbc39f84569d49af3b3e403c8f7f168
JakWai01 <salet@posteo.net>
2024-04-15 18:18:07 +0200

    feat: Read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{env, fs::File, io::{self, BufRead}, path::Path};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file_path: &String = &args[1];
+
+    println!("In file {}", file_path);
+
+    if let Ok(lines) = read_lines(file_path) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines.flatten() {
+            let parts = line.split(" ");
+            for part in parts {
+                println!("{}", part);
+            }
+        }
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors.
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }

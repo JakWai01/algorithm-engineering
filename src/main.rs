e9d5c288f00c1367da60ba48817ed60976847c6e
JakWai01 <salet@posteo.net>
2024-04-19 13:34:41 +0200

    feat: Finish connected component implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{env, fs::File, io::{self, BufRead}, path::Path, collections::HashMap};
-
+use std::{
+    collections::{HashMap, HashSet}, env, fs::File, io::{self, BufRead}, path::Path
+};
 
 // Consider using usizes in all fields
 #[derive(Debug)]
@@ -9,6 +10,7 @@ struct Vertex {
     lon: f64,
     lat: f64,
     height: usize,
+    marked: usize,
 }
 
 #[derive(Debug)]
@@ -26,7 +28,7 @@ fn main() {
 
     println!("In file {}", file_path);
 
-    let mut lines =  read_lines(file_path).unwrap();
+    let mut lines = read_lines(file_path).unwrap();
     for _ in 0..5 {
         lines.next();
     }
@@ -36,7 +38,7 @@ fn main() {
 
     println!("Number of vertices: {}", num_vertices);
     println!("Number of edges: {}", num_edges);
-
+    
     let mut vertices = HashMap::new();
     let mut edges: Vec<Edge> = Vec::new();
 
@@ -50,6 +52,7 @@ fn main() {
                 lon: iter.next().unwrap().parse().unwrap(),
                 lat: iter.next().unwrap().parse().unwrap(),
                 height: iter.next().unwrap().parse().unwrap(),
+                marked: 0,
             };
             // We don't need to store the whole vertex, lon and lat should be sufficient
             vertices.insert(vertex.id, vertex);
@@ -61,83 +64,97 @@ fn main() {
             let l = line.unwrap();
             let mut iter = l.split_whitespace();
 
+            let start_vertex = iter.next().unwrap().parse().unwrap();
+            let end_vertex = iter.next().unwrap().parse().unwrap();
+            let weight = iter.next().unwrap().parse().unwrap();
+            let typ = iter.next().unwrap().parse().unwrap();
+            let max_speed = iter.next().unwrap().parse().unwrap();
+            
             let edge = Edge {
-                start_vertex: iter.next().unwrap().parse().unwrap(),
-                end_vertex: iter.next().unwrap().parse().unwrap(),
-                weight: iter.next().unwrap().parse().unwrap(),
-                typ: iter.next().unwrap().parse().unwrap(),
-                max_speed: iter.next().unwrap().parse().unwrap()
+                start_vertex: start_vertex,
+                end_vertex: end_vertex,
+                weight: weight,
+                typ: typ,
+                max_speed: max_speed,
             };
             edges.push(edge);
+
+            if start_vertex != end_vertex {
+                let back_edge = Edge {
+                    start_vertex: end_vertex,
+                    end_vertex: start_vertex,
+                    weight: weight,
+                    typ: typ,
+                    max_speed: max_speed 
+                };
+                edges.push(back_edge);
+            }
         }
     }
 
     // Sort by starting node in order to get offset array
     edges.sort_by_key(|edge| edge.start_vertex);
-    
+
     // num_edges != edges.len() since we are converting the edges to an undirected graph
     // by inserting the edges another time in reverse direction
-    let mut offset_array: Vec<usize> = vec![edges.len(), num_vertices];
-    
+    let mut offset_array: Vec<usize> = vec![edges.len(); num_vertices];
+
     // Initialize variables
     let mut previous_vertex_id = 0;
-    offset_array.insert(0, 0);
-    
+    // offset_array.insert(0, 0);
+    offset_array[0] = 0;
+
     // If the the start_vertex changes in the edges vector, store the offset in the offset vector
     // and set this offset for all start_vertex id's that have been skipped in this last step.
     // However, I am not sure if this case even occurs in our road network.
-    for (i, edge) in edges.iter().enumerate() {
+    for (edge_index, edge) in edges.iter().enumerate() {
         if edge.start_vertex != previous_vertex_id {
-            for j in previous_vertex_id+1..=edge.start_vertex {
-                offset_array.insert(j, i);
+            for j in previous_vertex_id + 1..=edge.start_vertex {
+                offset_array[j] = edge_index;
             }
             previous_vertex_id = edge.start_vertex;
         }
     }
 
+    // println!("Offset array {:?}", offset_array);
+
     // Finding connected components of a graph using DFS
     // All vertices are initially initialized with a marker value of 0 which can be interpreted as
     // the component not being marked
     let mut c: usize = 0;
-    
-    // The HashMap is kind of useless, we only need the keys
-    let mut visited: HashMap<usize, usize> = HashMap::new();
-    
-    println!("Vertices elements {}", vertices.len());
-    for v in vertices.values() {
-        if !visited.contains_key(&v.id) {
-            println!("Key is not contained! Starting dfs");
+
+    let mut visited: HashSet<usize> = HashSet::new();
+
+    for v in vertices.values().into_iter() {
+        if !visited.contains(&v.id) {
+            // println!("So far, visited does not contain {}, hence executing dfs again. visited: {:?}", v.id, visited);
             c += 1;
             dfs(v, &mut visited, &offset_array, &edges);
-        } else {
-            println!("Vertex is already in a component");
         }
     }
 
     println!("Number of connected components {}", c);
-    
-    println!("First five offsets of offset array {:?}", &offset_array[0..300]);
-
-    println!("Last offsets of offset array {:?}", &offset_array.last());
-
-    println!("First five edges of edge array {:?}", &edges[0..100]);
-
-    println!("DONE");
 }
 
-fn dfs(v: &Vertex, visited: &mut HashMap<usize, usize>, offset_array: &Vec<usize>, edges: &Vec<Edge>) {
+fn dfs(
+    v: &Vertex,
+    visited: &mut HashSet<usize>,
+    offset_array: &Vec<usize>,
+    edges: &Vec<Edge>,
+) {
     let mut stack = Vec::new();
 
     stack.push(v.id);
-    visited.insert(v.id, 1);
-    
+    visited.insert(v.id);
+
     while !stack.is_empty() {
         if let Some(current_vertex) = stack.pop() {
             let neighbour_ids = get_neighbour_ids(current_vertex, offset_array, edges);
+            // println!("Neighbour_ids {:?}", neighbour_ids);
             for n in neighbour_ids {
-                if !visited.contains_key(&n) {
+                if !visited.contains(&n) {
                     stack.push(n);
-                    visited.insert(n, 1);
+                    visited.insert(n);
                 }
             }
         }
@@ -145,19 +162,26 @@ fn dfs(v: &Vertex, visited: &mut HashMap<usize, usize>, offset_array: &Vec<usize
 }
 
 fn get_neighbour_ids(vertex_id: usize, offset_array: &Vec<usize>, edges: &Vec<Edge>) -> Vec<usize> {
+    // println!("Get neighbours for vertex {}", vertex_id);
     let mut offset_index = *offset_array.get(vertex_id).unwrap();
     let mut neighbour_ids: Vec<usize> = Vec::new();
-    
-    let mut start_vertex = edges.get(offset_index).unwrap().start_vertex;
+
+    let mut start_vertex;
+
+    if let Some(edge) = edges.get(offset_index) {
+        start_vertex = edge.start_vertex;
+    } else {
+        return Vec::new();
+    }
 
     while start_vertex == vertex_id {
         let neighbour_id = edges.get(offset_index).unwrap().end_vertex;
-        neighbour_ids.push(neighbour_id);  
+        neighbour_ids.push(neighbour_id);
         offset_index += 1;
-        if let Some(start) = edges.get(offset_index) {
-            start_vertex = start.start_vertex;
+        if let Some(new_edge) = edges.get(offset_index) {
+            start_vertex = new_edge.start_vertex;
         } else {
-            return Vec::new()
+            break
         }
     }
 
@@ -167,7 +191,9 @@ fn get_neighbour_ids(vertex_id: usize, offset_array: &Vec<usize>, edges: &Vec<Ed
 // The output is wrapped in a Result to allow matching on errors.
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
